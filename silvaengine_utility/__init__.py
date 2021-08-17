d91e0b909985b73bedd9d6f7fd51a5ca4beb5ba5
Barry Liu <barry.y.liu@hotmail.com>
2021-08-17 10:40:36 +0800

    [A] Add new class

--- silvaengine_utility/__init__.py
+++ silvaengine_utility/__init__.py
@@ -2,8 +2,9 @@
 # -*- coding: utf-8 -*-
 __author__ = "bibow"
 
-__all__ = ["utility", "http", "graphql"]
-from .utility import Utility
-from .utility import Struct
+__all__ = ["utility", "http", "graphql", "authorizer"]
+
+from .utility import Utility, Struct
 from .http import HttpResponse
 from .graphql import Graphql, JSON
+from .authorizer import Authorizer
