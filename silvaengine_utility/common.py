9edbec0bd9fe9ac3ec3f207f87de39884f61c73a
Barry Liu <barry.y.liu@hotmail.com>
2021-09-28 21:52:04 +0800

    [U] Update

--- silvaengine_utility/common.py
+++ silvaengine_utility/common.py
@@ -13,12 +13,19 @@ import boto3, json
 class Common(object):
     @staticmethod
     def get_grouped_seller_role_emails(logger, role_types, relation_type, ids):
-        role_sellers = Auth(logger).get_users_by_role_type(
+        role_sellers = Utility.import_dynamically(
+            "silvaengine_auth",
+            "get_users_by_role_type",
+            "Auth",
+            constructor_parameters={"logger": logger},
+        )(
             role_types,  # 待查询的角色类别, 数组, 必填, 可取值为: 0 - 普通角色, 1 - GWI Account Manager, 2 - GWI QC Manager, 3 - Dept Managers
             relation_type,  # 关系类型, 整型, 必填, 可取值为:0 - admin, 1 - seller, 2 - team
             ids,  # 商家或公司ID, 数组, 选填, 默认为None
         )
+
         result = defaultdict(dict)
+
         for seller_role in role_sellers:
             if seller_role["role_id"] == "b874bcee-0af8-11ec-acc5-5d5264ad5593":
                 for seller_id, users in seller_role["groups"].items():
