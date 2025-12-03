c4e67d7ffe01cc64f94380c57436b11ccf83b7b1
Barry Liu <barry.y.liu@gmail.com>
2025-12-03 17:57:42 +0800

    [A] Init.

--- silvaengine_utility/__init__.py
+++ silvaengine_utility/__init__.py
@@ -6,6 +6,6 @@ __all__ = ["utility", "http", "graphql", "authorizer", "common"]
 
 from .authorizer import Authorizer
 from .common import Common
-from .graphql import JSON, Graphql
 from .http import HttpResponse
 from .utility import Struct, Utility
+# from .graphql import JSON, Graphql
