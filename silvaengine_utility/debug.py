bd866883a5a7d913ca87bc2eab786630d27e3869
Barry Liu <barry.y.liu@hotmail.com>
2026-01-06 16:13:33 +0800

    [U] Optimization

--- silvaengine_utility/debug.py
+++ silvaengine_utility/debug.py
@@ -7,16 +7,12 @@ class Debug(object):
     @staticmethod
     def info(info, variable, delimiter_title="", delimiter="-", delimiter_total=40):
         fn = print
+        is_valid_info = info and hasattr(info, "context") and "logger" in info.context
 
-        if (
-            info
-            and hasattr(info, "context")
-            and "logger" in info.context
-            and hasattr(info.context.get("logger"), "info")
-        ):
+        if is_valid_info and hasattr(info.context.get("logger"), "info"):
             fn = info.context.get("logger").info
 
-        if bool(info.get("context", {}).get("debug_mode", True)):
+        if not is_valid_info or bool(info.context.get("debug_mode", True)):
             t = f"{'-' * delimiter_total} {{mark}}: {str(delimiter_title).strip()} {'-' * delimiter_total}"
 
             fn(t.format(mark="START"))
