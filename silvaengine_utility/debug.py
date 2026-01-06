18f2f6c0f5564e5ed735a8760a6dcf1fb8ebb950
Barry Liu <barry.y.liu@hotmail.com>
2026-01-06 15:48:48 +0800

    [U] Optimization

--- silvaengine_utility/debug.py
+++ silvaengine_utility/debug.py
@@ -6,7 +6,15 @@ from __future__ import print_function
 class Debug(object):
     @staticmethod
     def info(info, variable, delimiter_title="", delimiter="-", delimiter_total=40):
-        fn = info.get("context", {}).get("logger") or print
+        fn = print
+
+        if (
+            info
+            and hasattr(info, "context")
+            and "logger" in info.context
+            and hasattr(info.context.get("logger"), "info")
+        ):
+            fn = info.context.get("logger").info
 
         if bool(info.get("context", {}).get("debug_mode", True)):
             fn(
