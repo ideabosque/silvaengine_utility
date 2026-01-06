eb6a64fc09ba0611e796b855b8e631ba0db519bd
Barry Liu <barry.y.liu@hotmail.com>
2026-01-06 15:38:56 +0800

    [U] Optimization

--- silvaengine_utility/debug.py
+++ silvaengine_utility/debug.py
@@ -6,15 +6,13 @@ from __future__ import print_function
 class Debug(object):
     @staticmethod
     def info(info, variable, delimiter_title="", delimiter="-", delimiter_total=40):
-        fn = print
+        fn = info.get("context", {}).get("logger") or print
 
-        if info and info.context and info.context.get("logger"):
-            fn = info.context.get("logger").info
-
-        fn(
-            f"{'-' * delimiter_total} START: {str(delimiter_title).strip()} {'-' * delimiter_total}"
-        )
-        fn(variable)
-        fn(
-            f"{'-' * (delimiter_total + 1)} END: {str(delimiter_title).strip()} {'-' * (delimiter_total + 1)}"
-        )
+        if bool(info.get("context", {}).get("debug_mode", True)):
+            fn(
+                f"{'-' * delimiter_total} START: {str(delimiter_title).strip()} {'-' * delimiter_total}"
+            )
+            fn(variable)
+            fn(
+                f"{'-' * (delimiter_total + 1)} END: {str(delimiter_title).strip()} {'-' * (delimiter_total + 1)}"
+            )
