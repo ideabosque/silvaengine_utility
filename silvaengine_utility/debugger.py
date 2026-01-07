65c764afd5b1888cefb99b2460adb8f3b76bcf8c
Barry Liu <barry.y.liu@hotmail.com>
2026-01-07 11:39:20 +0800

    [U] Optimization

---
+++ silvaengine_utility/debugger.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+
+import logging
+from typing import Any, Dict, Optional
+
+
+class Debugger(object):
+    @staticmethod
+    def info(
+        variable: Any,
+        stage: str = "",
+        delimiter: str = "-",
+        delimiter_repetitions: int = 40,
+        setting: Dict[str, Any] = {},
+        logger: Optional[logging.Logger] = None,
+    ):
+        fn = logger.info if isinstance(logger, logging.Logger) else print
+        is_debug_mode = (
+            setting.get("debug_model", True) if type(setting) is dict else True
+        )
+        delimiter_repetitions = (
+            int(delimiter_repetitions) if int(delimiter_repetitions) > 0 else 40
+        )
+        delimiter = str(delimiter).strip() if str(delimiter).strip() else "-"
+        stage = str(stage).strip()
+
+        if is_debug_mode:
+            t = f"{'-' * delimiter_repetitions} {{mark}}: {stage} {'-' * delimiter_repetitions}"
+
+            fn(t.format(mark="START"))
+            fn(variable)
+            fn(t.format(mark="END"))
