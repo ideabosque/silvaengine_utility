327c57a921db82295851d0c11feb1174be259ef9
Barry Liu <barry.y.liu@hotmail.com>
2026-01-14 17:25:20 +0800

    [U] Optimization

--- silvaengine_utility/http.py
+++ silvaengine_utility/http.py
@@ -15,7 +15,6 @@ class HttpResponse(object):
         data: Any,
         status_code: int = HttpStatus.OK.value,
         content_type: str = "application/json",
-        as_websocket_format: bool = False,
     ):
         content_type = (
             str(content_type).strip()
@@ -32,14 +31,3 @@ class HttpResponse(object):
             },
             "body": Serializer.json_dumps(data),
         }
-
-        # if not as_websocket_format:
-        #     response.update(
-        #         headers={
-        #             "Access-Control-Allow-Headers": "Access-Control-Allow-Origin",
-        #             "Access-Control-Allow-Origin": "*",
-        #             "Content-Type": content_type,
-        #         }
-        #     )
-
-        # return response
