21dd3a11c79ec8e665611002a681c3177f29d694
Barry Liu <barry.y.liu@hotmail.com>
2026-01-15 13:59:33 +0800

    [U] Optimization

--- silvaengine_utility/http.py
+++ silvaengine_utility/http.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function
 
-from typing import Any
+from typing import Any, Dict, Optional
 
 from silvaengine_constants import HttpStatus
 
@@ -14,6 +14,7 @@ class HttpResponse(object):
     def format_response(
         data: Any,
         status_code: int = HttpStatus.OK.value,
+        headers: Optional[Dict[str, Any]] = None,
         content_type: str = "application/json",
     ):
         content_type = (
@@ -22,12 +23,17 @@ class HttpResponse(object):
             else "application/json"
         )
 
+        response_headers = {
+            "Access-Control-Allow-Headers": "Access-Control-Allow-Origin",
+            "Access-Control-Allow-Origin": "*",
+            "Content-Type": content_type,
+        }
+
+        if isinstance(headers, dict):
+            response_headers.update(headers)
+
         return {
             "statusCode": int(status_code),
-            "headers": {
-                "Access-Control-Allow-Headers": "Access-Control-Allow-Origin",
-                "Access-Control-Allow-Origin": "*",
-                "Content-Type": content_type,
-            },
+            "headers": response_headers,
             "body": Serializer.json_dumps(data),
         }
