6fb7edcafabcb2f778d23856a89b35b187d7b967
Barry Liu <barry.y.liu@hotmail.com>
2026-09-10 20:02:23 +0800

    chore: remove debug print statement from Graphql.execute

--- silvaengine_utility/graphql.py
+++ silvaengine_utility/graphql.py
@@ -156,7 +156,6 @@ class Graphql(object):
             if isinstance(params.get("context"), dict):
                 context.update(**params.get("context", {}))
 
-            print(">>>>>>>>>>> Context:", context)
 
             query = params.get("query")
 
